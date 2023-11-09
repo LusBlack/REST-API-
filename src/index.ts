c3fbb87a83e5962cc804116a1e4c9249a02c9cfc
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-09 02:43:52 +0100

    reconfigured server and connected to mongodb

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
-
+import mongoose from 'mongoose';
 const app = express();
 
 app.use(cors({
@@ -17,6 +17,13 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log('Server running on  http://localhost:8080/');
-})
+server.listen(5000, () => {
+    console.log('Server running on  http://localhost:5000/');
+});
+
+
+const MONGO_URL ='mongodb+srv://louisikwuegbu3:BeachMaster02@cluster0.dfzuqzs.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error)); //test
