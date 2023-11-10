97717b8b578f21df6c1cc3a94fad3a27713c93a5
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-10 02:30:55 +0100

    imported router

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
 const app = express();
 
 app.use(cors({
@@ -27,3 +28,5 @@ const MONGO_URL ='mongodb+srv://Louis:BeachMaster02@rest.7vyzbzp.mongodb.net/?re
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log("wahala dey")); //test
+
+app.use('/', router());
