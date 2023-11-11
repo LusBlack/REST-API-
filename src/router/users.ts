a21ea2ceb8d39bb8719677445e79c12cc2fcde9b
Louis Ikwuegbu <Lou.ikwuegbu@gmail.com>
2023-11-11 19:06:17 +0100

    added routes for users

---
+++ src/router/users.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+import { deleteUser, getAllUsers } from '../controllers/users';
+import { isAuthenticated, isOwner } from '../middleware';
+import authentication from './authentication';
+
+export default (router: express.Router) => {
+    router.get('/users', isAuthenticated, getAllUsers); 
+    router.delete('/users/:id', isOwner, deleteUser);
+};
